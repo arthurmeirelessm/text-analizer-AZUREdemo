d3d5015aed227978c807bb55e30a5e755591bf9a
arthurmeirelessm <arthurmsesjdfdn@gmail.com>
2023-06-27 20:33:11 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Azure;
+using Azure.AI.TextAnalytics;
+using DotNetEnv;
+using System.Xml.Schema;
+
+public class Program
+{
+    private static readonly AzureKeyCredential keyCredential = new AzureKeyCredential(Environment.GetEnvironmentVariable("KEYCREDENTIAL"));
+    private static readonly Uri endpoint = new Uri(Environment.GetEnvironmentVariable("ENDPOINT"));
+
+    static void Main(string[] args)
+    {
+        DotNetEnv.Env.Load();
+        var test = Environment.GetEnvironmentVariable("ENDPOINT");
+        Console.WriteLine(test);
+    }
+}
