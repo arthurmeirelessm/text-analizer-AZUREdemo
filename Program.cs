a56aec026c66ca48c07d0c13b646da82409f35cd
arthurmeirelessm <arthurmsesjdfdn@gmail.com>
2023-06-28 01:07:15 -0300

    Integrate and comunicate code with language service in Azure

--- Program.cs
+++ Program.cs
@@ -11,7 +11,27 @@ public class Program
     static void Main(string[] args)
     {
         DotNetEnv.Env.Load();
-        var test = Environment.GetEnvironmentVariable("ENDPOINT");
-        Console.WriteLine(test);
+
+        var client = new TextAnalyticsClient(endpoint, keyCredential);
+
+        SentimentAnalysisExample(client);
     }
+
+
+    static void SentimentAnalysisExample(TextAnalyticsClient client)
+    {
+        string input = "dsggggsgdfffdfgssfdsdfsfsfdsffsfdfffsdfs";
+        DocumentSentiment documentSentiment = client.AnalyzeSentiment(input);
+        Console.WriteLine(documentSentiment.Sentiment);
+
+        foreach (var sentence in documentSentiment.Sentences)
+        {
+            Console.WriteLine($"\tText: \"{sentence.Text}\"");
+            Console.WriteLine($"\tSentence sentiment: \"{sentence.Sentiment}");
+            Console.WriteLine($"\tPositive score: \"{sentence.ConfidenceScores.Positive:0.00}");
+            Console.WriteLine($"\tNegative score: \"{sentence.ConfidenceScores.Negative:0.00}");
+            Console.WriteLine($"\tNeutral score: \"{sentence.ConfidenceScores.Neutral:0.00}\n");
+        }
+    }
+
 }
